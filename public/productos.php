4dd63c399a6c68b8cd50bc7412691de505e317cb
Dogus1386 <dogus1386@gmail.com>
2026-02-10 12:41:59 -0600

    Fix includes to work in local and production

--- public/productos.php
+++ public/productos.php
@@ -1,9 +1,11 @@
 <?php
 $pageTitle = "Productos";
-require __DIR__ . "/../app/layout/header.php";
+
+$PROJECT_ROOT = is_dir(__DIR__ . "/app") ? __DIR__ : dirname(__DIR__);
+require $PROJECT_ROOT . "/app/layout/header.php";
 ?>
 
 <h2>Productos</h2>
 <p>Esta será la pantalla de productos.</p>
 
-<?php require __DIR__ . "/../app/layout/footer.php"; ?>
+<?php require $PROJECT_ROOT . "/app/layout/footer.php"; ?>
