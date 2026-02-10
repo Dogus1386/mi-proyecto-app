4949f07bfb3dc8ba6ca4ffed2f03fb1557e4405e
Dogus1386 <139095795+Dogus1386@users.noreply.github.com>
2026-02-09 21:57:19 -0600

    Replace PHP output with HTML structure

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
-<?php
-echo "<h1>App funcionando ✅</h1>";
-echo "<p>Ruta pública: /public/index.php</p>";
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Proyecto App</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+    <h1>Mi aplicación ya está funcionando</h1>
+    <p>Deploy automático con Git y cPanel.</p>
+
+</body>
+</html>
