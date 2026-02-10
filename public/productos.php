7a53dd1b4e0d20a5bad810c8fc748e862edb20dd
Dogus1386 <139095795+Dogus1386@users.noreply.github.com>
2026-02-09 22:08:11 -0600

    Create productos.php for product display page

---
+++ public/productos.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "Productos";
+require __DIR__ . "/../app/layout/header.php";
+?>
+
+<h2>Productos</h2>
+<p>Esta será la pantalla de productos.</p>
+
+<?php require __DIR__ . "/../app/layout/footer.php"; ?>
