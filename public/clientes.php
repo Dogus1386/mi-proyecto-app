f11289e4e802b7504daeff6d89827299d77f0b76
Dogus1386 <139095795+Dogus1386@users.noreply.github.com>
2026-02-09 22:07:40 -0600

    Create clientes.php

---
+++ public/clientes.php
@@ -0,0 +1,9 @@
+<?php
+$pageTitle = "Clientes";
+require __DIR__ . "/../app/layout/header.php";
+?>
+
+<h2>Clientes</h2>
+<p>Esta será la pantalla de clientes.</p>
+
+<?php require __DIR__ . "/../app/layout/footer.php"; ?>
